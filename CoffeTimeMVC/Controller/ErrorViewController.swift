1fffa9e5facbaf349ec10ef3bef67cc9eed45ac1
andrrum <andrey.rumiantsev95@gmail.com>
2023-09-02 01:16:37 +0300

    setup sound

--- CoffeTimeMVC/Controller/ErrorViewController.swift
+++ CoffeTimeMVC/Controller/ErrorViewController.swift
@@ -7,10 +7,12 @@
 
 import Foundation
 import UIKit
+import AVFoundation
 
 class ErrorViewController: UIViewController {
     
     private var errorView = ErrorView()
+    private var player: AVAudioPlayer?
     
     override func loadView() {
         super.loadView()
@@ -25,7 +27,11 @@ class ErrorViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        playSound()
+    }
     
+    override func viewDidDisappear(_ animated: Bool) {
+        stopSound()
     }
     
     private func setupErrView() {
@@ -43,6 +49,41 @@ extension ErrorViewController: ErrorViewDelegate {
     }
     
     func closeButtonTapped() {
+        self.dismiss(animated: true)
+    }
+
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "rw", withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            if let player = player, player.isPlaying {
+                player.stop()
+                player.currentTime = 0
+            }
+
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            player?.delegate = self
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func stopSound() {
+        player?.stop()
+        player = nil
+    }
+}
+
+extension ErrorViewController: AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
         self.dismiss(animated: true)
     }
 }
